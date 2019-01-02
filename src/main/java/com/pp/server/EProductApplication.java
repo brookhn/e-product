9727d3720a8674cf71b52c8cd7fd92e4de78deca
yangtao <18111987@suningcultural.com>
2019-01-02 17:00:47 +0800

    test

---
+++ src/main/java/com/pp/server/EProductApplication.java
@@ -0,0 +1,14 @@
+package com.pp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EProductApplication.class, args);
+	}
+
+}
+
