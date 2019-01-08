5bcdd8a873cba5ad9318f93930160cc3e4b6cc6c
y_tao <y_tao@neusoft.com>
2019-01-08 23:51:44 +0800

    test

--- src/main/java/com/pp/server/config/RestConfiguration.java
+++ src/main/java/com/pp/server/config/RestConfiguration.java
@@ -1,10 +1,15 @@
 package com.pp.server.config;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class RestConfiguration {
@@ -17,8 +22,5 @@ public class RestConfiguration {
     {
         return builder.build();
     }
-
-
-
 
 }
--- src/main/java/com/pp/server/controller/WeatherController.java
+++ src/main/java/com/pp/server/controller/WeatherController.java
@@ -1,7 +1,10 @@
 package com.pp.server.controller;
 
+import brave.propagation.ExtraFieldPropagation;
 import com.pp.server.entity.WeatherResponse;
 import com.pp.server.service.WeatherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,18 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
+
+    Logger log = LoggerFactory.getLogger(WeatherController.class);
+
     @Autowired
     private WeatherService weatherService;
+
+    @GetMapping("/shlo")
+    public String helloSer()
+    {
+        String sessionId = ExtraFieldPropagation.get("SessionId");
+        System.out.println(sessionId);
+        return "hello,session is "+ ExtraFieldPropagation.get("SessionId");
+    }
 
     @GetMapping("/cityId/{cityId}")
     public WeatherResponse getReportByCityId(@PathVariable("cityId") String cityId)
     {
+        log.info("server receive param {}", cityId);
         return weatherService.getDataCityById(cityId);
     }
 
     @GetMapping("/cityName/{cityName}")
     public WeatherResponse getReportByCityName(@PathVariable("cityName") String cityName)
     {
+        log.info("server receive param {}", cityName);
         return weatherService.getDataCityByName(cityName);
     }
 }
